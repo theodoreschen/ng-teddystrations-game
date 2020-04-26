ce0ed75eff7075020591be62dbe4fafd46c9b6a3
theodoreschen <theodoreschen@gmail.com>
2020-04-26 09:25:21 -0400

    implemented backend polling

--- src/app/game-master/game-master.component.ts
+++ src/app/game-master/game-master.component.ts
@@ -1,13 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { LoggerService } from '../logger.service';
 import { GameStateService } from '../game-state.service';
+import { GameState } from '../game-server-types';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-game-master',
   templateUrl: './game-master.component.html',
   styleUrls: ['./game-master.component.css']
 })
-export class GameMasterComponent implements OnInit {
+export class GameMasterComponent implements OnInit, OnDestroy {
+  statePoller: any;
+  state: GameState;
+  develMode: boolean;
+  doStatePolling: boolean;
+  pollCounter: number;
 
   constructor(
     private log: LoggerService,
@@ -15,6 +22,33 @@ export class GameMasterComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.develMode = this.log.develMode;
+    this.doStatePolling = true;
+    this.startPolling();
+  }
+
+  ngOnDestroy(): void {
+    this.stopPolling();
+  }
+
+  startPolling(): void {
+    this.statePoller = interval(2000).subscribe(count => {
+      this.gameState.fetchGameState().subscribe(state => this.state = state);
+      this.pollCounter = count + 1;
+    });
+  }
+
+  stopPolling(): void {
+    this.statePoller.unsubscribe();
+  }
+
+  toggleStatePolling(): void {
+    if (this.doStatePolling) {
+      this.stopPolling();
+    } else {
+      this.startPolling();
+    }
+    this.doStatePolling = !this.doStatePolling;
   }
 
 }
