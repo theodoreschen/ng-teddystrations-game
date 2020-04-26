7d1675b40438bf2e9b977613b2a6b293e4568dba
theodoreschen <theodoreschen@gmail.com>
2020-04-26 01:25:49 -0400

    fixed unit tests

--- src/app/game-master/game-master.component.spec.ts
+++ src/app/game-master/game-master.component.spec.ts
@@ -1,14 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GameMasterComponent } from './game-master.component';
+import { LoggerService } from '../logger.service';
+import { GameStateService } from '../game-state.service';
 
 describe('GameMasterComponent', () => {
+  let mockLog, mockGameStateService;
   let component: GameMasterComponent;
   let fixture: ComponentFixture<GameMasterComponent>;
 
   beforeEach(async(() => {
+    mockLog = jasmine.createSpyObj(["DEBUG"]);
+    mockGameStateService = jasmine.createSpyObj(["fetchGameState"]);
+
     TestBed.configureTestingModule({
-      declarations: [ GameMasterComponent ]
+      declarations: [ GameMasterComponent ],
+      providers: [
+        { provide: LoggerService, useValue: mockLog },
+        { provide: GameStateService, useValue: mockGameStateService }
+      ]
     })
     .compileComponents();
   }));
--- src/app/game-state.service.spec.ts
+++ src/app/game-state.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { GameStateService } from './game-state.service';
 
 describe('GameStateService', () => {
+  let httpTestingController: HttpTestingController;
   let service: GameStateService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+
+    httpTestingController = TestBed.get(HttpTestingController);
     service = TestBed.inject(GameStateService);
   });
 
