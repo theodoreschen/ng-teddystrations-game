eb10d7de58995625a8b381e26cfae191eea61c41
tsc <theairportexplorer@gmail.com>
2020-06-05 11:03:59 -0400

    adding an additional unit test for ToTimePipe

--- src/app/to-time.pipe.spec.ts
+++ src/app/to-time.pipe.spec.ts
@@ -1,8 +1,14 @@
 import { ToTimePipe } from './to-time.pipe';
 
 describe('ToTimePipe', () => {
-  it('create an instance', () => {
+  it('should create an instance', () => {
     const pipe = new ToTimePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should properly format 61 seconds', () => {
+    const pipe = new ToTimePipe();
+    let fmtTime = pipe.transform(61);
+    expect(fmtTime).toEqual('1:01');
   });
 });
